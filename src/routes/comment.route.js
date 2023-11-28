3e2849dcf84c886120ba43f821b86e910ad00f42
KienTrungz <kien.bt207710@sis.hust.edu.vn>
2023-11-28 11:53:43 +0700

    providing isValidate for prevent from user who is not active

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const commentController = require('../controllers/comment.controller.js');
 const { isAuth } = require('../middlewares/auth.middleware.js');
+const { isValidate } = require('../middlewares/validated.middleware.js')
 
-router.post('/get_mark_comment', isAuth, commentController.getMarkComment);
-router.post('/set_mark_comment', isAuth, commentController.setMarkComment);
+router.post('/get_mark_comment', isAuth, isValidate, commentController.getMarkComment);
+router.post('/set_mark_comment', isAuth, isValidate,commentController.setMarkComment);
 
 module.exports = router;
